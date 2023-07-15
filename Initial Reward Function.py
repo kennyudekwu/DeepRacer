864189d84872497e61d383a7497cb3203fa22f8a
Maddogmax <53562899+Maddog-max@users.noreply.github.com>
2023-07-15 20:23:17 +0100

    Fixing Log 0 Error

--- Initial Reward Function.py
+++ Initial Reward Function.py
@@ -155,7 +155,7 @@ def reward_function(params):
     
     def progress_reward(progress, steps):
         PROGRESSION_RATE = 0.05
-        reward = (progress/100)**2 * (1/(PROGRESSION_RATE * math.log(steps) + 1))
+        reward = (progress/100)**2 * (1/(PROGRESSION_RATE * math.log(steps + 1) + 1))
         print("progress_reward: {}".format(reward))
         return reward
     
@@ -196,31 +196,31 @@ def reward_function(params):
 ########    
 # TEST #
 ########
-#
-#  test_state = {
-#     "all_wheels_on_track": True,                    # flag to indicate if the agent is on the track
-#     "x": 5.5,                                       # agent's x-coordinate in meters
-#     "y": 10.5,                                      # agent's y-coordinate in meters
-#     "closest_objects": [0, 1],                      # zero-based indices of the two closest objects to the agent's current position of (x, y).
-#     "closest_waypoints": [0, 1],                    # indices of the two nearest waypoints.
-#     "distance_from_center": 0.4,                    # distance in meters from the track center 
-#     "is_crashed": False,                            # Boolean flag to indicate whether the agent has crashed.
-#     "is_left_of_center": True,                      # Flag to indicate if the agent is on the left side to the track center or not. 
-#     "is_offtrack": False,                           # Boolean flag to indicate whether the agent has gone off track.
-#     "is_reversed": False,                           # flag to indicate if the agent is driving clockwise (True) or counter clockwise (False).
-#     "heading": 10.3,                                # agent's yaw in degrees
-#     "objects_distance": [10.2],                     # list of the objects' distances in meters between 0 and track_length in relation to the starting line.
-#     "objects_heading": [120.3],                     # list of the objects' headings in degrees between -180 and 180.
-#     "objects_left_of_center": [True, True],         # list of Boolean flags indicating whether elements' objects are left of the center (True) or not (False).
-#     "objects_location": [(16.2, 32.1),(5.1, 22.8)], # list of object locations [(x,y), ...].
-#     "objects_speed": [0],                           # list of the objects' speeds in meters per second.
-#     "progress": 50.2,                               # percentage of track completed
-#     "speed": 5,                                     # agent's speed in meters per second (m/s)
-#     "steering_angle": 21,                           # agent's steering angle in degrees
-#     "steps": 50000,                                 # number steps completed
-#     "track_length": 35.2,                           # track length in meters.
-#     "track_width": 1.3,                             # width of the track
-#     "waypoints": [(10.3, 12.4), (5.1, 6.4)]         # list of (x,y) as milestones along the track center
-# }
-# 
-# print("End reward: {}".format(reward_function(test_state)))
+
+test_state = {
+    "all_wheels_on_track": True,                    # flag to indicate if the agent is on the track
+    "x": 5.5,                                       # agent's x-coordinate in meters
+    "y": 10.5,                                      # agent's y-coordinate in meters
+    "closest_objects": [0, 1],                      # zero-based indices of the two closest objects to the agent's current position of (x, y).
+    "closest_waypoints": [0, 1],                    # indices of the two nearest waypoints.
+    "distance_from_center": 0.4,                    # distance in meters from the track center 
+    "is_crashed": False,                            # Boolean flag to indicate whether the agent has crashed.
+    "is_left_of_center": True,                      # Flag to indicate if the agent is on the left side to the track center or not. 
+    "is_offtrack": False,                           # Boolean flag to indicate whether the agent has gone off track.
+    "is_reversed": False,                           # flag to indicate if the agent is driving clockwise (True) or counter clockwise (False).
+    "heading": 10.3,                                # agent's yaw in degrees
+    "objects_distance": [10.2],                     # list of the objects' distances in meters between 0 and track_length in relation to the starting line.
+    "objects_heading": [120.3],                     # list of the objects' headings in degrees between -180 and 180.
+    "objects_left_of_center": [True, True],         # list of Boolean flags indicating whether elements' objects are left of the center (True) or not (False).
+    "objects_location": [(16.2, 32.1),(5.1, 22.8)], # list of object locations [(x,y), ...].
+    "objects_speed": [0],                           # list of the objects' speeds in meters per second.
+    "progress": 50.2,                               # percentage of track completed
+    "speed": 5,                                     # agent's speed in meters per second (m/s)
+    "steering_angle": 21,                           # agent's steering angle in degrees
+    "steps": 0,                                 # number steps completed
+    "track_length": 35.2,                           # track length in meters.
+    "track_width": 1.3,                             # width of the track
+    "waypoints": [(10.3, 12.4), (5.1, 6.4)]         # list of (x,y) as milestones along the track center
+}
+ 
+print("End reward: {}".format(reward_function(test_state)))
